2c67648602b47feb6f43f694d080c6b9018853cc
Hieu Nguyen <16521675@gm.uit.edu.vn>
2019-12-31 12:36:26 +0700

    first commit

---
+++ Masteri2c/Masteri2c.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include<string.h>
+
+void recdata(char * tp, byte * conte, unsigned int length)
+{
+  String topic = String(tp);
+  //byte -> char* -> String
+  //remove ID
+  int flag;
+  for (int i = 0; i < topic.length(); i++) {
+    if(topic.charAt(i) == '=') {
+      flag = i;
+    }
+  }
+  String newTopic = topic;
+  newTopic.remove(0, flag + 1);
+  String content = String((char*)conte);
+  content.remove(length);
+  //format message to send for UNO
+  String data = newTopic + "," + content;
+  Serial.println(data);
+  char mess[20];
+  data.toCharArray(mess,20);
+  Wire.beginTransmission(8); /* begin with device address 8 */
+  Wire.write(mess);
+  Wire.endTransmission();  
+}
+
+WiFiClient client;
+//server, port, [callback], client
+PubSubClient MQTT("m24.cloudmqtt.com", 15217, recdata, client);
+
+void setup() 
+{
+  Serial.begin(9600); /* begin serial for debug */
+  //connect wifi
+  WiFi.begin("MHHH","giahieu99");
+  //check wifi
+  while(1)
+  {
+    delay(100);
+    if(WiFi.status()==WL_CONNECTED)
+      break;
+  }
+  Serial.println("successfully connected wifi");
+  
+  //check connect server
+  while(1)
+  {
+    delay(500);
+    //connect with server
+    if(MQTT.connect("ESP8266", "tmlgemnz", "7fub13-eRIeR"))
+      break;
+  }
+  Serial.println("successfully connected server MQTT");
+  //control relay module follow topic
+  MQTT.subscribe("6=pump");
+  MQTT.subscribe("6=ppm");
+  MQTT.subscribe("6=waterIn");
+  MQTT.subscribe("6=waterOut");
+  MQTT.subscribe("6=mix");
+  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+}
+
+void loop() 
+{
+  String s = "";
+  MQTT.loop();
+  //MQTT.publish("al13","1");
+  Wire.beginTransmission(8); /* begin with device address 8 */
+
+
+  Wire.requestFrom(8, 32); /* request & read data of size 13 from slave */
+  while(Wire.available())
+  {
+    char c = Wire.read(); 
+    if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'||c=='.'||c=='=')
+      s+=c;
+  }
+
+  Serial.print(s);
+  Serial.println();
+  char buffer[32];
+  s.toCharArray(buffer, 32);
+  
+  MQTT.publish("update",buffer);
+  Wire.endTransmission();    /* stop transmitting */
+  delay(5000);
+} 
---
+++ Slavei2c/Slavei2c.ino
@@ -0,0 +1,278 @@
+#include <Wire.h>
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+const unsigned int TRIG_PIN = 13;
+const unsigned int ECHO_PIN = 12;
+const int DHTPIN = 2;       //Đọc dữ liệu từ DHT11 ở chân 2 trên mạch Arduino
+const int DHTTYPE = DHT11;  //Khai báo loại cảm biến, có 2 loại là DHT11 và DHT22
+const int RelayPpm = 7; //Relay Pha che
+const int RelayWaterIn = 8; // Relay Bom nuoc vao thung
+const int RelayWaterOut = 9;// Relay bom nuoc ra khoi thung
+const int RelayMix =10; // Relay motor quay tron hon hop dung dich ding duong
+const int RelayPump = 11; // Relay bom nuoc len cho dan thuy canh
+DHT dht(DHTPIN, DHTTYPE);
+const int Light =  A2;
+String a;
+const unsigned int BAUD_RATE = 9600;
+/////EC
+#define ONE_WIRE_BUS 5
+int R1 = 850;
+int Ra = 25; //Resistance of powering Pins
+int ECPin = A0;
+int ECGround = A1; //=> Cam vs GND
+int ECPower = A3;
+int relayStatus;
+int distance1;
+int SlaveReceived=0;
+//*********** Converting to ppm [Learn to use EC it is much better**************//
+// Hana      [USA]        PPMconverion:  0.5
+// Eutech    [EU]          PPMconversion:  0.64
+//Tranchen  [Australia]  PPMconversion:  0.7
+// Why didnt anyone standardise this?
+float PPMconversion = 0.7;
+//*************Compensating for temperature ************************************//
+//The value below will change depending on what chemical solution we are measuring
+//0.019 is generaly considered the standard for plant nutrients [google "Temperature compensation EC" for more info
+float TemperatureCoef = 0.019; //this changes depending on what chemical we are measuring
+//********************** Cell Constant For Ec Measurements *********************//
+//Mine was around 2.9 with plugs being a standard size they should all be around the same
+//But If you get bad readings you can use the calibration script and fluid to get a better estimate for K
+float K = 2.7;
+//************ Temp Probe Related *********************************************//
+OneWire oneWire(ONE_WIRE_BUS);// Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire);// Pass our oneWire reference to Dallas Temperature.
+float Temperature = 0;
+float EC = 0;
+float EC25 = 0;
+int ppm = 0;
+
+int checkppm = 0;
+float raw = 0;
+float Vin = 5;
+float Vdrop = 0;
+float Rc = 0;
+float buffer1 = 0;
+/////
+int distance;
+void setup() {
+  Wire.begin(8);                /* join i2c bus with address 8 */
+  Wire.onReceive(receiveEvent); /* register receive event */
+  Wire.onRequest(requestEvent); /* register request event */
+  pinMode(RelayPpm, OUTPUT); //7->11
+  pinMode(RelayWaterIn, OUTPUT);
+  pinMode(RelayWaterOut, OUTPUT);
+  pinMode(RelayPump, OUTPUT);
+  pinMode(RelayMix, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  /////////EC////////
+  pinMode(ECPin, INPUT);
+  pinMode(ECPower, OUTPUT); //Setting pin for sourcing current
+  pinMode(ECGround, OUTPUT); //setting pin for sinking current
+  digitalWrite(ECGround, LOW); //We can leave the ground connected permanantly
+  pinMode(Light, INPUT);
+  delay(100);// gives sensor time to settle
+  sensors.begin();
+  delay(100);
+  R1 = (R1 + Ra); // Taking into acount Powering Pin Resitance
+  //////////////////
+  Serial.begin(9600);           /* start ser-ial for debug */
+  dht.begin(); // Khởi động cảm biến DHT11, (Nhiệt độ, độ ẩm)
+//digitalWrite(RelayPpm, HIGH);
+//digitalWrite(RelayWaterIn, HIGH);
+//digitalWrite(RelayWaterOut, HIGH);
+//digitalWrite(RelayPump, HIGH);
+//digitalWrite(RelayMix, HIGH);
+}
+
+void loop() {
+  if(checkppm == 1){
+    digitalWrite(RelayPpm, HIGH);
+    delay(1000);
+    digitalWrite(RelayPpm, LOW);
+    checkppm = 0;
+  }
+  float h = dht.readHumidity();    //Đọc độ ẩm
+  float temp = dht.readTemperature(); //Đọc nhiệt độ
+  int lig = analogRead(Light);
+  Serial.print("anh sang: ");
+  Serial.print(lig);
+  Serial.print("Nhiet do: ");
+  Serial.println(temp);               //Xuất nhiệt độ
+  Serial.print("Do am: ");
+  Serial.println(h);               //Xuất độ ẩm
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  const unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  distance1 = duration / 29 / 2;
+  distance = 100- (distance1*100/22);
+  if(distance < 0){
+    distance = 0;
+    }
+  if(distance >100){
+    distance =100;
+    }
+  if (duration == 0) {
+    Serial.println("Warning: no pulse from sensor");
+  }
+  else {
+    Serial.print(distance1);
+    Serial.println(" cm");
+  }
+  sensors.requestTemperatures();// Send the command to get temperatures
+  Temperature = sensors.getTempCByIndex(0); //Stores Value in Variable
+  /////////////////////////////EC PPM
+  GetEC();          //Calls Code to Go into GetEC() Loop [Below Main Loop] dont call this more that 1/5 hhz [once every five seconds] or you will polarise the water
+  PrintReadings();  // Cals Print routine [below main loop]
+  // id, device_id, temperature, humidity, light, EC, PPM, water, pump
+  a = "6=" + (String)temp + "=" + (String)h + "=" +(String)lig + "=" + (String)EC25 + "=" + (String)ppm + "=" + (String)distance + "="  +(String)relayStatus;  //(String)Temperature+"="+
+  Serial.println("relayStatus");
+  Serial.println(relayStatus);
+  Serial.println(distance1);
+  Serial.println(distance);
+  ////////
+  delay(5000); // [Dont read Ec more than once every 5 seconds]
+}
+
+// function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+  String data;
+  while (0 < Wire.available()) {
+    char SlaveReceived = Wire.read();/* receive byte as a character */
+    data += SlaveReceived; /* print the character */
+  }
+  int flag;
+  for (int i = 0; i < data.length(); i++) {
+    if(data.charAt(i) == ',') {
+      flag = i;
+    }
+  }
+  String topic = data;
+  String mess = data;
+  topic.remove(flag);
+  mess.remove(0, flag + 1);
+  if (topic == "pump") {
+    if (mess == "1") {
+      //Turn on pump
+      Serial.println("Turn on pump");
+      digitalWrite(RelayPump, HIGH);
+      relayStatus=1;
+    }
+    else if (mess == "0") {
+      //Turn off pump
+      Serial.println("Turn off pump");
+      digitalWrite(RelayPump, LOW);
+      relayStatus=0;
+    }
+  }
+  else if (topic == "ppm") {
+     if (mess == "1") {
+      //Turn on ppm
+      Serial.println("Turn on ppm");
+        checkppm = 1;
+    }
+    else if (mess == "0") {
+      //Turn off ppm
+      Serial.println("Turn off ppm");
+      digitalWrite(RelayPpm, LOW);
+    }
+  }
+  else if (topic == "waterIn") {
+    if (mess == "1") {
+      //Turn on waterIn RelayWaterIn
+      Serial.println("Turn on waterIn");
+      digitalWrite(RelayWaterIn, HIGH);
+    }
+    else if (mess == "0") {
+      //Turn off waterIn
+      Serial.println("Turn off waterIn");
+      digitalWrite(RelayWaterIn, LOW);
+    }
+  }
+  else if (topic == "waterOut") {
+    if (mess == "1") {
+      //Turn on waterOut
+      Serial.println("Turn on waterOut");
+      digitalWrite(RelayWaterOut, HIGH);
+    }
+    else if (mess == "0") {
+      //Turn off waterOut
+      Serial.println("Turn off waterOut");
+      digitalWrite(RelayWaterOut, LOW);
+    }
+  }
+  else if (topic == "mix") {
+    if (mess == "1") {
+      //Turn on mix
+      Serial.println("Turn on mix");
+      digitalWrite(RelayMix, HIGH);
+    }
+    else if (mess == "0") {
+      //Turn off mix
+      Serial.println("Turn off mix");
+      digitalWrite(RelayMix, LOW);
+    }
+  }
+}
+
+// function that executes whenever data is requested from master
+void requestEvent() { /*send string on request */
+  char buffer[32];
+  a.toCharArray(buffer, 32);
+  Wire.write(buffer);
+}
+void GetEC() {
+  //*********Reading Temperature Of Solution *******************//
+  sensors.requestTemperatures();// Send the command to get temperatures
+  Temperature = sensors.getTempCByIndex(0); //Stores Value in Variable
+  
+  //************Estimates Resistance of Liquid ****************//
+  digitalWrite(ECPower, HIGH);
+  raw = analogRead(ECPin);
+  raw = analogRead(ECPin); // This is not a mistake, First reading will be low beause if charged a capacitor
+  digitalWrite(ECPower, LOW);
+
+  //***************** Converts to EC **************************//
+  Vdrop = (Vin * raw) / 1024.0;
+  Rc = (Vdrop * R1) / (Vin - Vdrop);
+  Rc = Rc - Ra; //acounting for Digital Pin Resitance
+  EC = 1000 / (Rc * K);
+  
+  //*************Compensating For Temperaure********************//
+  EC25  =  EC / (1 + TemperatureCoef * (Temperature - 21.0));
+  ppm = (EC25) * (PPMconversion * 1000);
+}
+//************************** End OF EC Function ***************************//
+
+
+
+
+//***This Loop Is called From Main Loop- Prints to serial usefull info ***//
+void PrintReadings() {
+  Serial.print("Rc: ");
+  Serial.print(Rc);
+  Serial.print(" EC: ");
+  Serial.print(EC25);
+  Serial.print(" Simens  ");
+  Serial.print(ppm);
+  Serial.print(" ppm  ");
+  Serial.print(Temperature);
+  Serial.println(" *C  ");
+
+
+  /*
+    //********** Usued for Debugging ************
+    Serial.print("Vdrop: ");
+    Serial.println(Vdrop);
+    Serial.print("Rc: ");
+    Serial.println(Rc);
+    Serial.print(EC);
+    Serial.println("Siemens");
+    //********** end of Debugging Prints *********
+  */
+};
